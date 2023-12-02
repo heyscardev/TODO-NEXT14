cf98194125ff4d7cf90da669e43ec0c7172e6557
Heyscar Romero <heyscarromero@gmail.com>
2023-12-02 01:01:08 -0300

    next forwarder port

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         remotePatterns: [{
             hostname: "tailus.io",protocol:"https",pathname:"/sources/blocks/stats-cards/preview/images/**"
-}]}}
+        }]
+    },
+    experimental: {
+    serverActions: {
+         allowedForwardedHosts: ["localhost", "gp71dwqz-3000.brs.devtunnels.ms"],
+      allowedOrigins: ["gp71dwqz-3000.brs.devtunnels.ms", "localhost:3000"]
+    }
+  }
+}
 
 module.exports = nextConfig
