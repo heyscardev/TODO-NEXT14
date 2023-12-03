45025266f49c3d328e51347fd306cb2adffac892
Heyscar Romero <heyscarromero@gmail.com>
2023-12-03 02:50:36 -0300

    products logic

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,9 +1,12 @@
 import { ProductCard } from "@/products";
+import { products } from "@/products/data/product";
 
 export default function ProductsPage() {
   return (
     <div className="bg-white rounded-lg p-4 grid grid-cols-1 sm:grid-cols-3 gap-2">
-      <ProductCard />
+      {products.map((product) => (
+        <ProductCard {...product} key={product.id} />
+      ))}
     </div>
   );
 }
