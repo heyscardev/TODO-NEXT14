54977c50b3b23693b529a1193d0a730fb63cc0c9
Heyscar Romero <heyscarromero@gmail.com>
2023-12-04 11:30:21 -0300

    dasboard items

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,20 @@
 import { WidgetItem } from "@/components";
+import { cookies } from "next/headers";
 import React from "react";
 
 const Dashboardpage = () => {
+  const cookiesCart = JSON.parse(cookies().get("cart")?.value ?? "{}");
+  const totalPoducts: number = Object.values(cookiesCart).reduce(
+    (prev: number, total: any) => prev + total,
+    0
+  );
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
-      <WidgetItem />
-      <WidgetItem />
-      <WidgetItem />
+      <WidgetItem
+        title="Total products in cart"
+        labelValue={totalPoducts}
+        subtitle="products to buy in shopping cart"
+      />
     </div>
   );
 };
